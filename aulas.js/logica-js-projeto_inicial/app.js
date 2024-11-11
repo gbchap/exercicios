fbbb722e0abb9b6c0bc01da441923eb7c54b6890
John Doe <gb.freitaschap@gmail.com>
2024-11-11 14:06:37 -0300

    js

---
+++ aulas.js/logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert('Boas-vindas ao jogo do número secreto!'); //cria um alerta
+let numeroSecreto = 5; //camelCase
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso aí! Você descobriu o número secreto!');
+}
