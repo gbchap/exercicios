6861d449b5fce319095dd7532ed715f908bf90aa
John Doe <gb.freitaschap@gmail.com>
2024-11-18 14:46:02 -0300

    jsmudancas

--- aulas.js/logica-js-projeto_inicial/app.js
+++ aulas.js/logica-js-projeto_inicial/app.js
@@ -1,13 +1,26 @@
 alert('Boas-vindas ao jogo do número secreto!'); //cria um alerta
 let numeroSecreto = 5; //camelCase
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (numeroSecreto == chute) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert ('Você errou!')
+//enquanto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    if (numeroSecreto == chute) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+    }
+    //mesma coisa de tentativas += 1
+    tentativas ++;
 }
-// live server faz com que não precise ficar 
+// live server (extensão que baixamos) faz com que não precise ficar 
 // reinicializando a pag pra testar o código
 
